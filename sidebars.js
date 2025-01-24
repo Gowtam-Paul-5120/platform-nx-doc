cdb04430f51de4ea9b85eef1af47608afbfd0f97
Gowtam Paul <gowtamp@ivoyant.com>
2025-01-24 13:20:34 +0530

    new project

---
+++ sidebars.js
@@ -0,0 +1,79 @@
+const sidebarData = require("./config/sidebar/index.json");
+
+const getDocId = (doc) => {
+  return doc
+
+    .replace(/\.mdx?$/, "")
+
+    .split("/")
+
+    .slice(1)
+
+    .join("/");
+};
+
+const getItem = (item) => {
+  const type = item["_template"];
+
+  let itemProps = {
+    type: type,
+  };
+
+  if (type === "doc") {
+    if (!item.document) {
+      return [];
+    }
+
+    itemProps.id = getDocId(item.document);
+
+    if (item.label) {
+      itemProps.label = item.label;
+    }
+  }
+
+  if (type === "category") {
+    if (item.title) {
+      itemProps.label = item.title;
+    }
+
+    if (item.link && item.link !== "none") {
+      if (item.link === "doc" && item.docLink) {
+        itemProps.link = {
+          type: "doc",
+
+          id: getDocId(item.docLink),
+        };
+      } else if (item.link === "generated") {
+        itemProps.link = {
+          type: "generated-index",
+        };
+      } else {
+        return [];
+      }
+    }
+
+    itemProps.items = Array.isArray(item.items)
+      ? item.items.flatMap((item) => getItem(item))
+      : [];
+  }
+
+  if (type === "link") {
+    if (item.href && item.title) {
+      itemProps.label = item.title;
+
+      itemProps.href = item.href;
+    } else {
+      return [];
+    }
+  }
+
+  return [itemProps];
+};
+
+const sidebars = {
+  tutorialSidebar: Array.isArray(sidebarData.items)
+    ? sidebarData.items.flatMap((item) => getItem(item))
+    : [],
+};
+
+module.exports = sidebars;
